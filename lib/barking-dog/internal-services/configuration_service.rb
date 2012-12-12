312ae648e4c0b7475267e8e442fc4efafcebccb6
Topper Bowers <topper@toppingdesign.com>
2012-12-12 10:30:41 -0500

    some more specs for the config service

--- lib/barking-dog/internal-services/configuration_service.rb
+++ lib/barking-dog/internal-services/configuration_service.rb
@@ -1,18 +1,26 @@
 module BarkingDog
   class ConfigurationService < BaseService
 
-    def initialize
-      subscribe("barking-dog.new_configuration", :handle_new_configuration)
+    DEFAULT_EVENT_PATH = "barking-dog.configuration"
+
+    attr_reader :event_path
+    def initialize(event_path =  DEFAULT_EVENT_PATH)
+      @event_path = event_path
+      subscribe("#{event_path}.new", :handle_new_configuration)
     end
 
     def handle_new_configuration(pattern, config)
       logger.debug("setting config to: #{config.inspect}")
       @configuration = config
-      publish("barking-dog.new_configuration_saved", @configuration)
+      fire("saved", @configuration)
     end
 
     def configuration
       @configuration ||= {}
+    end
+
+    def fire(event, *payload)
+      publish("#{event_path}.#{event}", payload)
     end
 
   end
--- spec/barking-dog/internal-services/configuration_service_spec.rb
+++ spec/barking-dog/internal-services/configuration_service_spec.rb
@@ -3,6 +3,8 @@ require 'barking-dog/internal-services/configuration_service'
 
 module BarkingDog
   describe ConfigurationService do
+    let(:receiver) { GlobalEventReceiver.new }
+
     before do
       @publisher = GlobalEventPublisher.new
       @config_service = ConfigurationService.new
@@ -11,17 +13,41 @@ module BarkingDog
     after do
       @publisher.terminate
       @config_service.terminate
+      receiver.terminate
+    end
+
+    it "should set the event namespace at initialize" do
+      new_config_service = ConfigurationService.new("custom.configuration")
+      new_config_service.event_path.should == "custom.configuration"
+      new_config_service.terminate
     end
 
     it "should have a default config" do
       @config_service.configuration.should == {}
+      @config_service.event_path.should == ConfigurationService::DEFAULT_EVENT_PATH
     end
 
-    it "should update configs on events" do
-      @publisher.publish("barking-dog.new_configuration", {:foo => :bar})
-      @config_service.configuration.should == {:foo => :bar}
-    end
+    describe "updating the config" do
+      let(:event_name) { "barking-dog.configuration.saved" }
+      let(:new_config) { {:foo => :bar} }
 
+      before do
+        @future = receiver.future.wait_for(event_name)
+        @publisher.publish("barking-dog.configuration.new", new_config)
+        @config_service.configuration.should == new_config
+      end
+
+      it "should update configs on events" do
+        @config_service.configuration.should == new_config
+      end
+
+      it "should publish config changes" do
+        val = @future.value(1)
+        val.first.should == event_name
+        val.last.should == [new_config]
+      end
+
+    end
   end
 
 
